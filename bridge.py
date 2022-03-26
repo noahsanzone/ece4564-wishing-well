f97db7999ba6aab1d24ed1fdab4d0030d91d8d23
jventura7 <82546770+jventura7@users.noreply.github.com>
2022-03-26 16:05:02 -0500

    test bluetooth connection

--- bridge.py
+++ bridge.py
@@ -1,10 +1,8 @@
-# import bluetooth
+import bluetooth
 import pymongo
 import pika
 import time
 import sys
-import bluetooth
-#import pybluez
 
 if (len(sys.argv) == 3) and (sys.argv[1] == "-s"):
     raspIP = sys.argv[2]
@@ -68,27 +66,22 @@ else:
 
 macADDR = '9c:b6:d0:b8:94:c8'
 
-"""
+
 # bluetooth connection
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind(("", bluetooth.PORT_ANY))
 server_sock.listen(1)
-
 port = server_sock.getsockname()[1]
-
-uuid = "52736A4D-C510-5515-BD49-3BAEA0EA30D1"
-
+uuid = "E106A78C-63F2-11E8-A4C3-9829A6584161"
 bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
                             service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
                             profiles=[bluetooth.SERIAL_PORT_PROFILE],
                             # protocols=[bluetooth.OBEX_UUID]
                             )
-
 print("Waiting for connection on RFCOMM channel", port)
-
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from", client_info)
-
+"""
 try:
     while True:
         data = client_sock.recv(1024)
@@ -97,9 +90,7 @@ try:
         print("Received", data)
 except OSError:
     pass
-
 print("Disconnected.")
-
 client_sock.close()
 server_sock.close()
 print("All done.")
