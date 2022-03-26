9ea667c7591112a5239e036aee12bd5a475ab9e2
noahsanzone <60456771+noahsanzone@users.noreply.github.com>
2022-03-26 19:41:23 -0400

    Add files via upload

--- bridge.py
+++ bridge.py
@@ -7,6 +7,25 @@ import sys
 if (len(sys.argv) == 3) and (sys.argv[1] == "-s"):
     raspIP = sys.argv[2]
 
+    macADDR = '9c:b6:d0:b8:94:c8'
+
+    # bluetooth connection
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock.bind(("", bluetooth.PORT_ANY))
+    server_sock.listen(1)
+    port = server_sock.getsockname()[1]
+    uuid = "E106A78C-63F2-11E8-A4C3-9829A6584161"
+    #uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
+                                service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
+                                profiles=[bluetooth.SERIAL_PORT_PROFILE],
+                                # protocols=[bluetooth.OBEX_UUID]
+                                )
+    while True:
+        print("Waiting for connection on RFCOMM channel", port)
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from", client_info)
+
     # Establish RabbitMQ connection
     credentials = pika.PlainCredentials('admin', 'password')
     parameters = pika.ConnectionParameters(raspIP,
@@ -64,23 +83,7 @@ else:
     print("ERROR: Invalid Input")
 
 
-macADDR = '9c:b6:d0:b8:94:c8'
 
-
-# bluetooth connection
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", bluetooth.PORT_ANY))
-server_sock.listen(1)
-port = server_sock.getsockname()[1]
-uuid = "E106A78C-63F2-11E8-A4C3-9829A6584161"
-bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
-                            service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-                            profiles=[bluetooth.SERIAL_PORT_PROFILE],
-                            # protocols=[bluetooth.OBEX_UUID]
-                            )
-print("Waiting for connection on RFCOMM channel", port)
-client_sock, client_info = server_sock.accept()
-print("Accepted connection from", client_info)
 """
 try:
     while True:
