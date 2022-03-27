ab1e10eff0f05b54a4b02da587a6cd592ccf01d8
noahsanzone <60456771+noahsanzone@users.noreply.github.com>
2022-03-26 20:48:11 -0400

    Add files via upload

--- bridge.py
+++ bridge.py
@@ -9,22 +9,20 @@ if (len(sys.argv) == 3) and (sys.argv[1] == "-s"):
 
     macADDR = '9c:b6:d0:b8:94:c8'
 
-    # bluetooth connection
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-    server_sock.bind(("", bluetooth.PORT_ANY))
+
+    port = 4
+    server_sock.bind(("", port))
     server_sock.listen(1)
-    port = server_sock.getsockname()[1]
-    uuid = "E106A78C-63F2-11E8-A4C3-9829A6584161"
-    #uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-    bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
-                                service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
-                                profiles=[bluetooth.SERIAL_PORT_PROFILE],
-                                # protocols=[bluetooth.OBEX_UUID]
-                                )
-    while True:
-        print("Waiting for connection on RFCOMM channel", port)
-        client_sock, client_info = server_sock.accept()
-        print("Accepted connection from", client_info)
+
+    client_sock, address = server_sock.accept()
+    print("Accepted connection from ", address)
+
+    data = client_sock.recv(1024)
+    print("received [%s]" % data)
+
+    client_sock.close()
+    server_sock.close()
 
     # Establish RabbitMQ connection
     credentials = pika.PlainCredentials('admin', 'password')
