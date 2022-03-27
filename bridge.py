48088efb2476f292b1093635b4ec5a0e69d78ac9
jventura7 <82546770+jventura7@users.noreply.github.com>
2022-03-26 19:55:52 -0500

    established bluetooth with raspberry pi

--- bridge.py
+++ bridge.py
@@ -4,25 +4,19 @@ import pika
 import time
 import sys
 
+
+def callback(ch, method, properties, body):
+    print("%r:%r" % (method.routing_key, body))
+
+
 if (len(sys.argv) == 3) and (sys.argv[1] == "-s"):
     raspIP = sys.argv[2]
 
-    macADDR = '9c:b6:d0:b8:94:c8'
-
+    # Establish bluetooth connection
     server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-
     port = 4
     server_sock.bind(("", port))
     server_sock.listen(1)
-
-    client_sock, address = server_sock.accept()
-    print("Accepted connection from ", address)
-
-    data = client_sock.recv(1024)
-    print("received [%s]" % data)
-
-    client_sock.close()
-    server_sock.close()
 
     # Establish RabbitMQ connection
     credentials = pika.PlainCredentials('admin', 'password')
@@ -34,65 +28,60 @@ if (len(sys.argv) == 3) and (sys.argv[1] == "-s"):
     connection = pika.BlockingConnection(parameters)
     channel = connection.channel()
 
-    Place = 'Squires'
-    Subject = 'Food'
-    Message = 'This food sucks!'
-
-    channel.exchange_declare(exchange=Place, exchange_type='direct')
-
-    channel.basic_publish(exchange=Place,
-                          routing_key=Subject,
-                          body=Message)
-
-    connection.close()
-
-    # Send messages through mongoDB
-    s1 = "p:Squires+Rooms “I like the comfortable chairs on 3rd floor”"
-    s2 = "c:Goodwin+Rooms “John sucks”"
-    s3 = "c:Goodwin+Rooms “Karthik is racist”"
-    stringList = [s1, s2, s3]
-
-    # Producer command
+    # Establish MongoDB connection
     connect = pymongo.MongoClient()
-    for s in stringList:
-        action = s[0]
-        warehouse = s[2:s.find('+')]
-        msgID = msgID = "{team}'$'{ticks}".format(team="14", ticks=time.time())
 
-        # go to specified database
-        db = connect[warehouse]
+    while True:
+        print("Waiting for connection on RFCOMM channel", port)
+        client_sock, client_info = server_sock.accept()
+        print("Accepted connection from", client_info)
+        while True:
+            # Message from bluetooth connection
+            s = client_sock.recv(1024)
+            print("received [%s]" % data)
+            if not s:
+                break
+            action = s[0]
 
-        collection = s[s.find('+') + 1: s.find(' ')]
-        text = s[s.find(' ') + 1:]
+            # Check and perform valid action and send to mongoDB
+            if action == 'p' or action == 'c':
+                warehouse = s[2:s.find('+')]
+                collection = s[s.find('+') + 1: s.find(' ')]
+                text = s[s.find(' ') + 1:]
+                msgID = msgID = "{team}'$'{ticks}".format(team="14", ticks=time.time())
 
-        # create collection
-        myCollection = connect[warehouse][collection]
+                # go to specified database
+                db = connect[warehouse]
 
-        data = {
-            "Action": action,
-            "Place": warehouse,
-            "MsgID": msgID,
-            "Subject": collection,
-            "Message": text
-        }
+                # create collection and send to MongoDB
+                myCollection = connect[warehouse][collection]
+                data = {
+                    "Action": action,
+                    "Place": warehouse,
+                    "MsgID": msgID,
+                    "Subject": collection,
+                    "Message": text
+                }
+                myCollection.insert_one(data)
 
-        myCollection.insert_one(data)
+                if action == 'p':
+                    channel.exchange_declare(exchange=warehouse,
+                                             exchange_type='direct')
+
+                    channel.basic_publish(exchange=warehouse,
+                                          routing_key=collection,
+                                          body=text)
+                else:
+                    channel.queue_bind(exchange=warehouse,
+                                       queue=collection,
+                                       routing_key=collection)
+
+                    channel.basic_consume(callback,
+                                          queue=collection,
+                                          no_ack=True)
+
+                    channel.start_consuming()
+            else:
+                print('ERROR: Incorrect Action')
 else:
     print("ERROR: Invalid Input")
-
-
-
-"""
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if not data:
-            break
-        print("Received", data)
-except OSError:
-    pass
-print("Disconnected.")
-client_sock.close()
-server_sock.close()
-print("All done.")
-"""
